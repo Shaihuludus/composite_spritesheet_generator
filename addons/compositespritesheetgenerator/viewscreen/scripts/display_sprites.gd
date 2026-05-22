b59b4efcc680edb19883f9acad98e799a3ec3631
Daniel Madejek <dmadejek@gmail.com>
2026-05-22 20:37:46 +0200

    cleanup and readme

--- addons/compositespritesheetgenerator/viewscreen/scripts/display_sprites.gd
+++ addons/compositespritesheetgenerator/viewscreen/scripts/display_sprites.gd
@@ -2,43 +2,29 @@
 class_name DisplaySprites extends Node2D
 
 @onready var sprite_sheet_main_view: Sprite2D = $SpriteSheetMainView
-@onready var sub_viewport = $SubViewport
+@onready var sub_viewport: SubViewport = $SubViewport
 
-var child_sprites: Array[String] = []
+var child_sprites: Array[Sprite2D] = []
 
 func delete_child_sprite(sprite_index: int):
+	child_sprites[sprite_index].queue_free()
 	child_sprites.remove_at(sprite_index)
-	reloadMainView()
-	
-func add_child_sprite(file_name: String):	
-	child_sprites.append(file_name)
-	reloadMainView()
 
-func reloadMainView():
-	var children: Array[Node] = sprite_sheet_main_view.get_children()
-	for child in children:
-		sprite_sheet_main_view.remove_child(child)
-		child.queue_free()		
-	for file_name in child_sprites:	
-		var childSprite: Sprite2D = Sprite2D.new() 
-		childSprite.texture = load(file_name)	
-		sprite_sheet_main_view.add_child(childSprite)		
+func add_child_sprite(file_name: String):
+	var child_sprite: Sprite2D = Sprite2D.new()
+	child_sprite.texture = load(file_name)
+	sprite_sheet_main_view.add_child(child_sprite)
+	child_sprites.append(child_sprite)
 
 func set_main_sprite(file_name: String):
 	sprite_sheet_main_view.texture = load(file_name)
-	sprite_sheet_main_view.position.x = sprite_sheet_main_view.texture.get_size().x / 2
-	sprite_sheet_main_view.position.y = sprite_sheet_main_view.texture.get_size().y / 2
-	reloadMainView()
+	sprite_sheet_main_view.position = sprite_sheet_main_view.texture.get_size() / 2
 
 func set_sprite_index(sprite_index: int, z_index: int):
-	if sprite_sheet_main_view:
-		sprite_sheet_main_view.get_child(sprite_index).z_index = z_index
+	child_sprites[sprite_index].z_index = z_index
 
 func set_sprite_x_offset(sprite_index: int, offset: int):
-	if sprite_sheet_main_view:
-		sprite_sheet_main_view.get_child(sprite_index).position.x = offset	
+	child_sprites[sprite_index].position.x = offset
 
 func set_sprite_y_offset(sprite_index: int, offset: int):
-	if sprite_sheet_main_view:
-		sprite_sheet_main_view.get_child(sprite_index).position.y = offset				
-	
+	child_sprites[sprite_index].position.y = offset
